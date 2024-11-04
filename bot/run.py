bf0453e3069c9885a2954dc9ebb10fda328fb176
denisnaenko <ngrinnbi@gmail.com>
2024-11-04 17:17:16 +0500

    Добавил асинхронную функцию cmd_start()

--- bot/run.py
+++ bot/run.py
@@ -2,6 +2,8 @@ import asyncio
 import os
 
 from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 from dotenv import load_dotenv
 
 # загружаем токен из .env
@@ -11,6 +13,11 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Bot(token = BOT_TOKEN)
 dp = Dispatcher()
+
+# обработка команды /start
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer('Бот запущен!')
 
 async def main():
     await dp.start_polling(bot)
