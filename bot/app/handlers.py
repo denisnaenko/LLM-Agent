19a9d10bd9b323f0745d8dd586b3c281ebeced60
denisnaenko <ngrinnbi@gmail.com>
2024-11-08 01:01:28 +0500

    Добавил inline меню для опции 'Начать анализ'

--- bot/app/handlers.py
+++ bot/app/handlers.py
@@ -2,7 +2,7 @@ import app.keyboards as kb
 
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 
 router = Router()
@@ -13,6 +13,10 @@ async def cmd_start(message: Message):
     await message.answer(f'Привет! Я - твой персональный ассистент по уходу за кожей лица.\n\nЯ помогу тебе выбрать лучшие косметические средства, основываясь на их составе, а также подберу индивидуальные рекомендации по уходу за кожей.\n\nЧем могу помочь?',
                          reply_markup=kb.main_menu)
 
+@router.message(lambda message: message.text == "Начать анализ")
+async def start_analysis(message: Message):
+    await message.answer("Выберите действие для анализа состава:", reply_markup=kb.analysis_menu)
+
 # обработка команды /help
 @router.message(Command('help')) 
 async def get_help(message: Message):
@@ -22,3 +26,8 @@ async def get_help(message: Message):
 @router.message(F.photo)
 async def get_photo(message: Message):
     await message.answer(f"ID фото: {message.photo[-1].file_id}")
+
+# обработка callback (start_analysis)
+@router.callback_query(F.data == "start_analysis")
+async def start_analysis(callback: CallbackQuery):
+    await callback.message.answer("Начинаю анализ...")    
--- bot/app/keyboards.py
+++ bot/app/keyboards.py
@@ -1,8 +1,17 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
+                           InlineKeyboardMarkup, InlineKeyboardButton)
 
-main_menu = ReplyKeyboardMarkup(keyboard=[
+# Главное меню - ReplyKeyboardMarkup
+main_menu = ReplyKeyboardMarkup(keyboard=[                        
     [KeyboardButton(text="Начать анализ")],
     [KeyboardButton(text="Персональные рекомендации")],
-    [KeyboardButton(text="История анализов"), KeyboardButton(text="Настройки")]
+    [KeyboardButton(text="История анализов")],
+    [KeyboardButton(text="Настройки")]
 ], resize_keyboard=True, input_field_placeholder="Выберите пункт меню:")
 
+# Inline меню для опции "Начать анализ"
+analysis_menu = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Загрузить фото упаковки", callback_data="upload_photo")],
+    [InlineKeyboardButton(text="Использовать текстовый ввод", callback_data="text_input")],
+    [InlineKeyboardButton(text="Назад в меню", callback_data="back_to_main")]]
+)
