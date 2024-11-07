c4a12dac0514612d37db5ce97bc11c6d4c0e7832
denisnaenko <ngrinnbi@gmail.com>
2024-11-07 23:28:37 +0500

    Изменил размер кнопок и добавил placeholder

--- bot/app/keyboards.py
+++ bot/app/keyboards.py
@@ -4,5 +4,5 @@ main_menu = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="Начать анализ")],
     [KeyboardButton(text="Персональные рекомендации")],
     [KeyboardButton(text="История анализов"), KeyboardButton(text="Настройки")]
-])
+], resize_keyboard=True, input_field_placeholder="Выберите пункт меню:")
 
