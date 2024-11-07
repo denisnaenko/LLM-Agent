adcf002c5714e722cafbe0a76b72acb40d29c8be
denisnaenko <ngrinnbi@gmail.com>
2024-11-08 01:44:10 +0500

    Удалил лишние функции обработки. Разметил файл handlers.py на шаблоны для последующего заполнения

--- bot/app/handlers.py
+++ bot/app/handlers.py
@@ -18,10 +18,18 @@ async def cmd_start(message: Message):
 async def start_analysis(message: Message):
     await message.answer("Выберите действие для анализа состава:", reply_markup=kb.analysis_menu)
 
+# обработка опции "Начать анализ" -> "Загрузить фото упаковки"
+
+# обработка опции "Начать анализ" -> "Использовать текстовый ввод"
+
 # обработка опции "Персональные рекомендации"
 @router.message(lambda message: message.text == "Персональные рекомендации")
 async def personal_rec(message: Message):
     await message.answer("Выберите действие для персональных рекомендаций:", reply_markup=kb.personal_rec_menu)
+
+# обработка опции "Персональные рекомендации" -> "Ввести данные о коже"
+
+# обработка опции "Персональные рекомендации" -> "Получить рекомендации"
 
 # обработка опции "История анализов"
 @router.message(lambda message: message.text == "История анализов")
@@ -33,17 +41,15 @@ async def analysis_history(message: Message):
 async def settings(message: Message):
     await message.answer("Настройки:", reply_markup=kb.settings_menu)
 
+# обработка опции "Настройки" -> "Обновить информацию о коже"
+
+# обработка опции "Настройки" -> "Включить/выключить уведомления"
+
+# обработка опции "Настройки" -> "Удалить историю анализов"
+
+# обработка опции ... -> "Назад в меню"
+
 # обработка команды /help
 @router.message(Command('help')) 
 async def get_help(message: Message):
     await message.answer("Это команда /help")
-
-# обработка фотографий
-@router.message(F.photo)
-async def get_photo(message: Message):
-    await message.answer(f"ID фото: {message.photo[-1].file_id}")
-
-# обработка callback (start_analysis)
-@router.callback_query(F.data == "start_analysis")
-async def start_analysis(callback: CallbackQuery):
-    await callback.message.answer("Начинаю анализ...")    
