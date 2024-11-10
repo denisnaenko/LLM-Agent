3a0008a6b4e1c04bf6d5393967be5769c1503494
denisnaenko <ngrinnbi@gmail.com>
2024-11-10 15:17:24 +0500

    Добавил модуль 'greenlet'

--- bot/app/database/models.py
+++ bot/app/database/models.py
@@ -1,6 +1,21 @@
+from sqlalchemy import BigInteger, String
 from sqlalchemy.orm import (DeclarativeBase, Mapped, 
                             mapped_column)
 from sqlalchemy.ext.asyncio import (AsyncAttrs, async_sessionmaker,
                                     create_async_engine)
 
+engine = create_async_engine(url="sqlite+aiosqlite:///db.sqlite3")
+async_session = async_sessionmaker(engine)
 
+class Base(AsyncAttrs, DeclarativeBase): ...
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tg_id = mapped_column(BigInteger)
+    skin_type: Mapped[str] = mapped_column(String(50))
+
+async def async_main():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
