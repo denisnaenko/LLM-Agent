7bedfa5bdaa1f4f22a22e94e95370fff0faa115f
denisnaenko <ngrinnbi@gmail.com>
2024-11-10 14:48:45 +0500

    Импортировал необходимые библиотеки для работы с БД

---
+++ bot/app/database/models.py
@@ -0,0 +1,6 @@
+from sqlalchemy.orm import (DeclarativeBase, Mapped, 
+                            mapped_column)
+from sqlalchemy.ext.asyncio import (AsyncAttrs, async_sessionmaker,
+                                    create_async_engine)
+
+
