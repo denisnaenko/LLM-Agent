489f73563f613461a23df058c49c61d0d43b58c5
denisnaenko <ngrinnbi@gmail.com>
2024-11-10 18:01:12 +0500

    Добавил request set_skin_type()

--- bot/app/database/requests.py
+++ bot/app/database/requests.py
@@ -9,3 +9,13 @@ async def set_user(tg_id):
         if not user:
             session.add(User(tg_id=tg_id))
             await session.commit()
+
+async def set_skin_type(tg_id, skin_type):
+    async with async_session() as session:
+        user = await session.scalar(select(User).where(User.tg_id == tg_id))
+
+        if user: 
+            user.skin_type = skin_type
+            await session.commit()
+        
+        
