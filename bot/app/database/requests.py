3958c994ee0b48b374cefbf3e1a4f4f95e6e05f3
denisnaenko <ngrinnbi@gmail.com>
2025-01-13 00:28:16 +0500

    Обновлён запрос к БД. Добавлена функция upsert_user

--- bot/app/database/requests.py
+++ bot/app/database/requests.py
@@ -1,21 +1,31 @@
 from app.database.models import async_session
 from app.database.models import User
-from sqlalchemy import select, update, delete
+from typing import List
+from sqlalchemy import text
 
-async def set_user(tg_id):
+async def upsert_user(tg_id: int, skin_type: str, features: List[str], risks: List[str]):
+    """Добавляет пользователя или обновляет данные, если запись уже существует"""
+    
     async with async_session() as session:
-        user = await session.scalar(select(User).where(User.tg_id == tg_id))
+        async with session.begin():
+            # Пытаемся получить пользователя по tg_id
+            query = text("SELECT * FROM users WHERE tg_id = :tg_id")
+            existing_user = await session.execute(query, {"tg_id": tg_id})
+            user = existing_user.fetchone()
 
-        if not user:
-            session.add(User(tg_id=tg_id))
-            await session.commit()
+            if user:
+                # Если пользователь существует, обновляем его данные
+                user_obj = await session.get(User, user.user_id)
+                user_obj.skin_type = skin_type
+                user_obj.set_features(features)
+                user_obj.set_risks(risks)
 
-async def set_skin_type(tg_id, skin_type):
-    async with async_session() as session:
-        user = await session.scalar(select(User).where(User.tg_id == tg_id))
-
-        if user: 
-            user.skin_type = skin_type
-            await session.commit()
-        
-        
+                print(f"Данные пользователя с tg_id={tg_id} обновлены.")
+            else:
+                # Если пользователь не найден, добавляем новую запись
+                user_obj = User(tg_id=tg_id, skin_type=skin_type)
+                user_obj.set_features(features)
+                user_obj.set_risks(risks)
+                session.add(user_obj)
+                
+                print(f"Пользователь с tg_id={tg_id} добавлен в базу.")
