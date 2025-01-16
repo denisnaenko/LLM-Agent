d699b1f5c0d9cbae6c71392e8f3bd17089731764
denisnaenko <ngrinnbi@gmail.com>
2025-01-16 05:10:32 +0500

    Удалён неиспользуемый код

--- bot/app/services/llama_request.py
+++ bot/app/services/llama_request.py
@@ -25,18 +25,3 @@ def query_llm(prompt):
         return f"Ошибка запроса: {e}"
     except json.JSONDecodeError as e:
         return f"Ошибка декодирования JSON: {e}"
-
-
-if __name__ == '__main__':
-    prompt = f""" Пользователь сказал: "Я бы хотел узнать какая у меня кожа". Выбери одно из действий:
-    1. upload_photo - Анализировать состав по фото.
-    2. text_input - Анализ текста состава.
-    3. get_skin_type - Определить тип/состояние кожи.
-    4. get_recommendations - Получить рекомендации по уходу.
-    Верни только название действия (upload_photo, text_input, get_skin_type, get_recommendations).
-    """
-    
-
-    result = query_llm(prompt)
-
-    if "get_skin_type" in result: print(result)
